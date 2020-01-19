0f802972cbf95fdda01540483905d48cb8d6411d
tamaki-osamu <tamaki.osamu0101@gmail.com>
2020-01-19 17:43:03 +0900

    Fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let now = new Date();
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <h2>{now.toLocaleTimeString()}</h2>
       </header>
     </div>
   );
