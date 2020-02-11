7fbc3e3d0d4b6baea0f70f57e357b8618dbe6fa8
tamaki-osamu <tamaki.osamu0101@gmail.com>
2020-02-12 00:08:19 +0900

    Add App-bar of Material-ui for navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,70 @@
 import React from "react";
-import "../App.css";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Slide from "@material-ui/core/Slide";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import logo from "../img/logo1.svg";
 
-const Navbar = () => (
-  <nav className="App-navbar">
-    Edit <code>src/App.js</code> and save to reload.
-  </nav>
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(4)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
-export default Navbar;
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func
+};
+
+export default function HideAppBar(props) {
+  const classes = useStyles();
+
+  return (
+    <nav>
+      <HideOnScroll {...props}>
+        <AppBar style={{ backgroundColor: "#454754" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <img src={logo} alt="logo" style={{ height: "50px" }} />
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+    </nav>
+  );
+}
