57f7238ae4fa14efa97be5cd73a8844c987d8952
tamaki-osamu <tamaki.osamu0101@gmail.com>
2020-03-15 01:27:15 +0900

    Update datas for table component

--- src/scripts/fetch_tidedata.py
+++ src/scripts/fetch_tidedata.py
@@ -4,6 +4,12 @@ import ephem
 import json
 
 
+def convert_tidetype_into_moonage(moon_age):
+    dic = {3: "中潮", 4: "中潮", 5: "中潮", 6: "中潮", 12: "中潮", 13: "中潮", 18: "中潮", 19: "中潮", 20: "中潮", 21: "中潮", 27: "中潮", 28: "中潮", 7: "小潮", 8: "小潮", 9: "小潮", 22: "小潮", 23: "小潮", 24: "小潮", 10: "長潮", 25: "長潮", 11: "若潮", 26: "若潮"
+           }
+    return dic.get(int(moon_age), "大潮")
+
+
 def extract_1month(target_year, target_month, point, lat, lon):
     url = "https://www.data.jma.go.jp/gmd/kaiyou/data/db/tide/suisan/txt/" + \
         str(target_year) + "/" + point + ".txt"
@@ -44,8 +50,10 @@ def extract_1month(target_year, target_month, point, lat, lon):
                     "%H:%M")
                 date_data["sunset"] = ephem.localtime(here.next_setting(sun)).strftime(
                     "%H:%M")
-                date_data["moon_age"] = here.date - \
-                    ephem.previous_new_moon(here.date)
+                moon_age = here.date - ephem.previous_new_moon(here.date)
+                date_data["tidetype"] = convert_tidetype_into_moonage(moon_age)
+                date_data["date"] = datetime(year, month, day).strftime(
+                    "%m/%d")
                 data[datetime(year, month, day).strftime(
                     "%Y/%m/%d")] = date_data
     return data
@@ -68,4 +76,6 @@ def fetch_tidedata_2month(point, name, lat, lon):
 if __name__ == "__main__":
     with open('public/test.json', 'w') as f:
         json.dump(fetch_tidedata_2month(
-            "KR", "稚内", 42.59, 144.22), f, ensure_ascii=False, indent=None)
+            "KR", "稚内", 42.59, 144.22), f, ensure_ascii=False, indent=4)
+
+    # print(convert_tidetype_into_moonage(3.5))
