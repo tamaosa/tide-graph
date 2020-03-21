b059d2e39c81edd4a20bf0108e5d619f1525fdb5
tamaki-osamu <tamaki.osamu0101@gmail.com>
2020-03-22 03:10:38 +0900

    mak it possible to get location in home page(#5)

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,32 +2,55 @@ import React from "react";
 import MapPoint from "./MapPoint";
 import "../App.css";
 
-const Home = () => (
-  <div>
-    <div className="title-content">
-      <div className="title-text">
-        <h1>Tidey</h1>
-        <p>とにかく「シンプル」なタイドグラフ</p>
+class Home extends React.Component {
+  state = {
+    lat: 36,
+    lon: 138,
+    zoom: 3
+  };
+
+  render() {
+    navigator.geolocation.getCurrentPosition(
+      pos =>
+        this.setState({
+          lat: pos.coords.latitude,
+          lon: pos.coords.longitude,
+          zoom: 9
+        }),
+      err => console.log(err)
+    );
+
+    return (
+      <div>
+        <div className="title-content">
+          <div className="title-text">
+            <h1>Tidey</h1>
+            <p>とにかく「シンプル」なタイドグラフ</p>
+          </div>
+        </div>
+        <div className="explanation-content">
+          <p>
+            Tideyでは全国239地点のシンプルなタイトグラフを配信しています。釣り
+            <span role="img" aria-label="fishing">
+              🎣🎣
+            </span>
+            やサーフィン
+            <span role="img" aria-label="surfing">
+              🏄🏄
+            </span>
+            のお供にぜひどうぞ。
+          </p>
+        </div>
+        <div className="map-content">
+          <h3>地域を探す</h3>
+          <MapPoint
+            data={[this.state.lat, this.state.lon]}
+            zoom={this.state.zoom}
+          />
+        </div>
       </div>
-    </div>
-    <div className="explanation-content">
-      <p>
-        Tideyでは全国239地点のシンプルなタイトグラフを配信しています。釣り
-        <span role="img" aria-label="fishing">
-          🎣🎣
-        </span>
-        やサーフィン
-        <span role="img" aria-label="surfing">
-          🏄🏄
-        </span>
-        のお供にぜひどうぞ。
-      </p>
-    </div>
-    <div className="map-content">
-      <h3>地域を探す</h3>
-      <MapPoint data={[36, 138]} zoom={3} />
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Home;
