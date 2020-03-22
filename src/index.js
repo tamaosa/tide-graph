b4368952b9a6aa3684a5ecfb16eee644ba052fc2
tamaki-osamu <tamaki.osamu0101@gmail.com>
2020-03-22 18:27:11 +0900

    Add support react-ga

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactGA from "react-ga";
+import createBrowserHistory from "history/createBrowserHistory";
+
+ReactGA.initialize("UA-154193009-2");
+const history = createBrowserHistory();
+history.listen(({ pathname }) => {
+  ReactGA.set({ page: pathname });
+  ReactGA.pageview(pathname);
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
