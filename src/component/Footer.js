db78a2b5a218073d3de5a32c7a50ea71d56700af
Tamaosa <tamaki.osamu0101@gmail.com>
2020-10-06 08:23:43 +0900

    Remove contact in footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
 import "../App.css";
 import logo_light from "../img/logo2.svg";
 import logo_dark from "../img/logo2_dark.svg";
@@ -10,14 +9,11 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     width: "80%",
     maxWidth: "720px",
+    paddingBottom: "1rem",
   },
   img: {
     width: "75%",
     maxWidth: "240px",
-  },
-  contact: {
-    textAlign: "right",
-    paddingBottom: "1rem",
   },
 }));
 
@@ -33,19 +29,6 @@ const Footer = (props) => {
           <img src={logo} alt="logo" className={classes.img} />
           <Typography>
             本ページで使用している潮汐データには、気象庁ホームページにて公開されている潮位表（天文潮位）を利用しています。
-          </Typography>
-        </div>
-        <div className={classes.contact}>
-          <Typography>
-            Contact&nbsp;
-            <Link
-              href="https://twitter.com/tamaki_osamu"
-              target="_blank"
-              rel="noopener noreferrer"
-              alt="twitter"
-            >
-              @Tamaosa
-            </Link>
           </Typography>
         </div>
       </div>
